511f325cd8f3c33638c141ff50df4fb0e2354c74
Aleksandra Kuper <aleksandra.kuper@wp.pl>
2017-07-30 14:53:27 +0200

    Three The Best List - displaying

--- Pack4Travel/Controllers/HomeController.cs
+++ Pack4Travel/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pack4Travel.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +9,18 @@ namespace Pack4Travel.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
+            var ThreeTheBestEquipementList = db.equipements.OrderByDescending(e => e.five_stars).Take(3).ToList();
+            
             if (Request.IsAuthenticated)
             {
                 return RedirectToAction("Index", "equipements");
             }
-            return View();
+            return View(ThreeTheBestEquipementList);
         }
-
 
         public ActionResult Login()
         {
